dd433d1baa77868ff98ecf57064e9c4c94be58ad
Tomasz Beneś <tbenes@anaconda.com>
2025-03-17 12:25:25 +0000

    update activate.sh

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -1,5 +1,16 @@
-export CONDA_BACKUP_JAVA_HOME="${JAVA_HOME}:-"
-export JAVA_HOME="${CONDA_PREFIX}"
+# Copied from conda-forge
 
-export CONDA_BACKUP_JAVA_LD_LIBRARY_PATH="${JAVA_LD_LIBRARY_PATH}:-"
-export JAVA_LD_LIBRARY_PATH="${JAVA_HOME}"/lib/server
+# First backup the varialbles if they are set.
+# The variables are allowed to be empty (Null).
+# Then set the variables to the location of this package.
+# The deactivate script restores the backed up variables.
+
+if [ "${JAVA_HOME+x}" ] ; then
+  export JAVA_HOME_CONDA_BACKUP="${JAVA_HOME}"
+fi
+export JAVA_HOME="${CONDA_PREFIX}/lib/jvm"
+
+if [ "${JAVA_LD_LIBRARY_PATH+x}" ] ; then
+  export JAVA_LD_LIBRARY_PATH_BACKUP="${JAVA_LD_LIBRARY_PATH}"
+fi
+export JAVA_LD_LIBRARY_PATH="${JAVA_HOME}/lib/server"
