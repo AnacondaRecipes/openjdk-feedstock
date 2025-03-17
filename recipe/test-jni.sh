cd95576398136845499109caed7ef9fed542eb97
Tomasz Beneś <tbenes@anaconda.com>
2025-03-17 14:18:31 +0000

    fix typo

--- recipe/test-jni.sh
+++ recipe/test-jni.sh
@@ -11,7 +11,7 @@ ls $JAVA_LD_LIBRARY_PATH
 os=$(uname -s | tr '[:upper:]' '[:lower:]')
 ${CC}                                 \
   -v                                  \
-  -xLinker -v                         \
+  -Xlinker -v                         \
   -I${JAVA_HOME}/include              \
   -I${JAVA_HOME}/include/$os          \
   -L${JAVA_LD_LIBRARY_PATH}           \
