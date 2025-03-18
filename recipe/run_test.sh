027c10d4a05a6b59139c11b5d48393b9f06e4450
Tomasz Beneś <tbenes@anaconda.com>
2025-03-18 15:12:18 +0000

    try osx<11 and whitelist missing dso

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -e
 
-# if [[ $target_platform == osx-64 ]]; then
-  # echo DEBUG_libSystem.tbd_START
-  # cat /Library/Developer/CommandLineTools/SDKs/MacOSX11.1.sdk/usr/lib/libSystem.tbd
-  # echo DEBUG_libSystem.tbd_END
-# fi
-
 if [ "${JAVA_HOME}" != "${PREFIX}" ] && [ "${JAVA_HOME}" != "${PREFIX}/Library" ]; then
   echo "ERROR: JAVA_HOME (${JAVA_HOME}) not equal to PREFIX (${PREFIX}) or ${PREFIX}/Library"
   exit 1
--- recipe/test-jni.sh
+++ recipe/test-jni.sh
@@ -6,21 +6,10 @@ if [[ ! -d $JAVA_LD_LIBRARY_PATH ]]; then
   exit 1
 fi
 
-OSX_EXTRA_ARG=""
-if [[ $target_platform == osx-64 ]]; then
-  OSX_EXTRA_ARG="-mmacosx-version-min=13.0"
-fi
-
-ls $JAVA_LD_LIBRARY_PATH
-
-echo "DEVELOPER_DIR: $DEVELOPER_DIR"
-echo "CONDA_BUILD_SYSROOT: $CONDA_BUILD_SYSROOT"
-
 os=$(uname -s | tr '[:upper:]' '[:lower:]')
 ${CC}                                 \
   -v                                  \
   -Xlinker -v                         \
-  $OSX_EXTRA_ARG                      \
   -I${JAVA_HOME}/include              \
   -I${JAVA_HOME}/include/$os          \
   -L${JAVA_LD_LIBRARY_PATH}           \
