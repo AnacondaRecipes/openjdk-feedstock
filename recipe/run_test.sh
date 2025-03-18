9aa1b6cf53d917334858b30a066f446e023a8cb3
Tomasz Beneś <tbenes@anaconda.com>
2025-03-18 14:38:40 +0000

    fix run_test

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-if [[ $target_platform == osx-64 ]]; then
+# if [[ $target_platform == osx-64 ]]; then
   # echo DEBUG_libSystem.tbd_START
   # cat /Library/Developer/CommandLineTools/SDKs/MacOSX11.1.sdk/usr/lib/libSystem.tbd
   # echo DEBUG_libSystem.tbd_END
-fi
+# fi
 
 if [ "${JAVA_HOME}" != "${PREFIX}" ] && [ "${JAVA_HOME}" != "${PREFIX}/Library" ]; then
   echo "ERROR: JAVA_HOME (${JAVA_HOME}) not equal to PREFIX (${PREFIX}) or ${PREFIX}/Library"
