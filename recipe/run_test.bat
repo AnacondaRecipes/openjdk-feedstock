c21e95fd9bf3848b7cc6908911c0e21bb74125e1
vkhomits <khomitsvalia@gmail.com>
2026-05-28 14:56:38 +0300

    Add checking files

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -9,3 +9,107 @@ pushd test-nio
   java -jar TestFilePaths.jar TestFilePaths.java
   IF ERRORLEVEL 1 exit 1
 popd
+
+@echo off
+echo Checking binaries...
+for %%f in (
+    Library\bin\jar.exe
+    Library\bin\jarsigner.exe
+    Library\bin\java.exe
+    Library\bin\javac.exe
+    Library\bin\javadoc.exe
+    Library\bin\javap.exe
+    Library\bin\javaw.exe
+    Library\bin\jcmd.exe
+    Library\bin\jconsole.exe
+    Library\bin\jdb.exe
+    Library\bin\jdeprscan.exe
+    Library\bin\jdeps.exe
+    Library\bin\jfr.exe
+    Library\bin\jhsdb.exe
+    Library\bin\jimage.exe
+    Library\bin\jinfo.exe
+    Library\bin\jlink.exe
+    Library\bin\jmap.exe
+    Library\bin\jmod.exe
+    Library\bin\jpackage.exe
+    Library\bin\jps.exe
+    Library\bin\jrunscript.exe
+    Library\bin\jshell.exe
+    Library\bin\jstack.exe
+    Library\bin\jstat.exe
+    Library\bin\jstatd.exe
+    Library\bin\jwebserver.exe
+    Library\bin\keytool.exe
+    Library\bin\rmiregistry.exe
+    Library\bin\serialver.exe
+) do (
+    if not exist "%PREFIX%\%%f" (
+        echo MISSING binary: %%f
+        exit /b 1
+    )
+)
+
+echo Checking libraries...
+for %%f in (
+    Library\bin\attach.dll
+    Library\bin\awt.dll
+    Library\bin\dt_socket.dll
+    Library\bin\extnet.dll
+    Library\bin\fontmanager.dll
+    Library\bin\freetype.dll
+    Library\bin\instrument.dll
+    Library\bin\j2gss.dll
+    Library\bin\j2pcsc.dll
+    Library\bin\j2pkcs11.dll
+    Library\bin\jaas.dll
+    Library\bin\java.dll
+    Library\bin\javajpeg.dll
+    Library\bin\jawt.dll
+    Library\bin\jdwp.dll
+    Library\bin\jimage.dll
+    Library\bin\jli.dll
+    Library\bin\jpackage.dll
+    Library\bin\jsound.dll
+    Library\bin\jsvml.dll
+    Library\bin\lcms.dll
+    Library\bin\le.dll
+    Library\bin\management.dll
+    Library\bin\management_agent.dll
+    Library\bin\management_ext.dll
+    Library\bin\mlib_image.dll
+    Library\bin\net.dll
+    Library\bin\nio.dll
+    Library\bin\prefs.dll
+    Library\bin\rmi.dll
+    Library\bin\saproc.dll
+    Library\bin\splashscreen.dll
+    Library\bin\sspi_bridge.dll
+    Library\bin\sunmscapi.dll
+    Library\bin\syslookup.dll
+    Library\bin\verify.dll
+    Library\bin\zip.dll
+    Library\bin\server\jvm.dll
+) do (
+    if not exist "%PREFIX%\%%f" (
+        echo MISSING library: %%f
+        exit /b 1
+    )
+)
+
+echo Checking headers...
+for %%f in (
+    Library\include\classfile_constants.h
+    Library\include\jawt.h
+    Library\include\jdwpTransport.h
+    Library\include\jni.h
+    Library\include\jvmti.h
+    Library\include\jvmticmlr.h
+) do (
+    if not exist "%PREFIX%\%%f" (
+        echo MISSING header: %%f
+        exit /b 1
+    )
+)
+
+echo All files present.
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -15,3 +15,111 @@ pushd test-nio
   jar cfm TestFilePaths.jar manifest.mf TestFilePaths.class
   java -jar TestFilePaths.jar TestFilePaths.java
 popd
+
+echo "Checking binaries..."
+for f in \
+    bin/jar \
+    bin/jarsigner \
+    bin/java \
+    bin/javac \
+    bin/javadoc \
+    bin/javap \
+    bin/jcmd \
+    bin/jconsole \
+    bin/jdb \
+    bin/jdeprscan \
+    bin/jdeps \
+    bin/jfr \
+    bin/jhsdb \
+    bin/jimage \
+    bin/jinfo \
+    bin/jlink \
+    bin/jmap \
+    bin/jmod \
+    bin/jpackage \
+    bin/jps \
+    bin/jrunscript \
+    bin/jshell \
+    bin/jstack \
+    bin/jstat \
+    bin/jstatd \
+    bin/jwebserver \
+    bin/keytool \
+    bin/rmiregistry \
+    bin/serialver \
+; do
+    if [ ! -f "$PREFIX/$f" ]; then
+        echo "MISSING binary: $f"
+        exit 1
+    fi
+done
+echo "Checking libraries..."
+case "$(uname -s)" in
+    Darwin)
+        lib_ext="dylib"
+        extra_libs="lib/libfreetype.$lib_ext lib/libawt_lwawt.$lib_ext lib/libosx.$lib_ext lib/libosxapp.$lib_ext lib/libosxkrb5.$lib_ext lib/libosxsecurity.$lib_ext lib/libosxui.$lib_ext"
+        ;;
+    Linux)
+        lib_ext="so"
+        extra_libs="lib/libawt_headless.$lib_ext lib/libawt_xawt.$lib_ext lib/libjsvml.$lib_ext lib/libmlib_image.$lib_ext lib/libsctp.$lib_ext lib/libsaproc.$lib_ext"
+        ;;
+esac
+
+for f in \
+    $lib_dir/libattach.$lib_ext \
+    $lib_dir/libawt.$lib_ext \
+    $lib_dir/libdt_socket.$lib_ext \
+    $lib_dir/libextnet.$lib_ext \
+    $lib_dir/libfontmanager.$lib_ext \
+    $lib_dir/libinstrument.$lib_ext \
+    $lib_dir/libj2gss.$lib_ext \
+    $lib_dir/libj2pcsc.$lib_ext \
+    $lib_dir/libj2pkcs11.$lib_ext \
+    $lib_dir/libjaas.$lib_ext \
+    $lib_dir/libjava.$lib_ext \
+    $lib_dir/libjavajpeg.$lib_ext \
+    $lib_dir/libjawt.$lib_ext \
+    $lib_dir/libjdwp.$lib_ext \
+    $lib_dir/libjimage.$lib_ext \
+    $lib_dir/libjli.$lib_ext \
+    $lib_dir/libjsig.$lib_ext \
+    $lib_dir/libjsound.$lib_ext \
+    $lib_dir/liblcms.$lib_ext \
+    $lib_dir/lible.$lib_ext \
+    $lib_dir/libmanagement.$lib_ext \
+    $lib_dir/libmanagement_agent.$lib_ext \
+    $lib_dir/libmanagement_ext.$lib_ext \
+    $lib_dir/libnet.$lib_ext \
+    $lib_dir/libnio.$lib_ext \
+    $lib_dir/libprefs.$lib_ext \
+    $lib_dir/librmi.$lib_ext \
+    $lib_dir/libsplashscreen.$lib_ext \
+    $lib_dir/libsyslookup.$lib_ext \
+    $lib_dir/libverify.$lib_ext \
+    $lib_dir/libzip.$lib_ext \
+    $lib_dir/server/libjsig.$lib_ext \
+    $lib_dir/server/libjvm.$lib_ext \
+    $extra_libs \
+; do
+    if [ ! -f "$PREFIX/$f" ]; then
+        echo "MISSING library: $f"
+        exit 1
+    fi
+done
+
+echo "Checking headers..."
+for f in \
+    $include_dir/classfile_constants.h \
+    $include_dir/jawt.h \
+    $include_dir/jdwpTransport.h \
+    $include_dir/jni.h \
+    $include_dir/jvmti.h \
+    $include_dir/jvmticmlr.h \
+; do
+    if [ ! -f "$PREFIX/$f" ]; then
+        echo "MISSING header: $f"
+        exit 1
+    fi
+done
+
+echo "All files present."
